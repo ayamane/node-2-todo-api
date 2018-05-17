422e0c9072314013bc86be8b62a44d021c637b96
andrewy <andrew.yamane@gmail.com>
2018-05-16 14:54:04 -1000

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,24 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET /todos/12342342
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // validate ID using isValid
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch ((e) => res.status(400).send());
 });
 
 app.listen(3000, () => {
