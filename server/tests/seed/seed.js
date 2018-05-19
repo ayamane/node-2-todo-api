3511645688ac4f1e464b7a293d2636da6cd39811
andrewy <andrew.yamane@gmail.com>
2018-05-19 07:43:43 -1000

    Add authentication to routes and tests

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -20,17 +20,23 @@ const users = [{
 }, {
   _id: userTwoId,
   email: 'john@example.com',
-  password: 'userTwoPass'
+  password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+  }]
 }];
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 444
+  completedAt: 444,
+  _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
